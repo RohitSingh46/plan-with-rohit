441cb423d5068711f04ca88f8b75490649ee2b3f
Rohit Singh <11rohitchs@gmail.com>
2024-05-03 12:02:02 +0530

    first commit

---
+++ src/components/Tours.js
@@ -0,0 +1,24 @@
+
+import Card from "./Card"
+function Tours({tours,removeTour})
+{
+    return(
+        <div className="container">
+            <div>
+              <h2 className="title">Plan With Rohit</h2>
+            </div>
+
+            <div className="cards">
+              {
+                tours.map((tour)=>{
+                    return <Card {...tour} removeTour={removeTour}  key={tour.id}></Card>
+                })
+              }  
+            </div>
+
+
+        </div>
+    )
+}
+
+export default Tours
